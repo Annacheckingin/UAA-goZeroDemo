1f16a11139a6065a88467b811936f46985ab9e37
lizhengguo <1519794332@qq.com>
2024-09-04 12:34:07 +0800

    feat:initial

---
+++ greet/internal/handler/greethandler.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"net/http"
+
+	"greet/internal/logic"
+	"greet/internal/svc"
+	"greet/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func GreetHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewGreetLogic(r.Context(), svcCtx)
+		resp, err := l.Greet(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			resp.Message = "Hello" + r.Context().Value("name").(string)
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
